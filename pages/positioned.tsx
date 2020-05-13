992624cca8011069ef4c265a2795f7fb0779bcf3
WenjunHuang <wenjun.huang80@gmail.com>
2020-05-13 15:09:20 +0800

    update

---
+++ pages/positioned.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export default () => {
+    return (
+        <Container>
+            <Container2>
+                <NavigationBar>
+                    <NavigationContent>Navigation Bar</NavigationContent>
+                </NavigationBar>
+                <BackgroundImage>Background Image</BackgroundImage>
+                <Features>Features</Features>
+            </Container2>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+  background-color:#dd6b20;
+  border: 0.25rem black solid;
+  height: 1024px;
+`
+
+
+const Container2 = styled.div`
+  background-color:white;
+  margin: 2rem;
+  border: 0.25rem black solid;
+`
+
+const Base = styled.div`
+  background-color: darkgray;
+  border: 0.25rem black solid;
+  color: white;
+  padding: 1rem;
+  margin:0.25rem;
+`
+const NavigationBar = styled(Base)`
+    position:fixed;
+    left: 30px;
+    top: 300px;
+`
+
+const NavigationContent = styled(Base)`
+  position: absolute;
+  left: 10px;
+  top:10px;
+`
+
+const BackgroundImage = styled(Base)``
+
+const Features = styled(Base)``
--- pages/styled.tsx
+++ pages/styled.tsx
@@ -3,13 +3,66 @@ import styled from 'styled-components';
 export default () => (
     <FlexContainer>
         <Item1>div</Item1>
+        <Item2>w = 250px</Item2>
+        <Item3>h = 250px</Item3>
+        <Item4>w/h = 300px</Item4>
+        <Item5>w = 350px</Item5>
+        <Item6>w = 350px</Item6>
     </FlexContainer>
 );
 
 const FlexContainer = styled.div`
-  display:flex;
-`;
+background:white;
+padding: 2rem;
+border: 0.25rem solid black;
+display: flex;
+height: 1300px;
+flex-direction:row;
+flex-wrap: nowrap;
+align-items:center;
+justify-content: flex-start;
+`
 
-const Item1 = styled.div`
-  
-`;
+const BaseItem = styled.div`
+border: 0.25rem black solid;
+box-sizing: border-box;
+padding: 1rem;
+color:white;
+font-weight: bold;
+font-size: 2rem;
+margin: 0.25rem;
+`
+
+const Item1 = styled(BaseItem)`
+background-color: #dd6b20;
+`
+
+const Item2 = styled(BaseItem)`
+width: 250px;
+background-color: #d69e2e;
+`
+
+const Item3 = styled(BaseItem)`
+  height: 250px;
+    margin-top: 1rem;
+    background-color: #d69e2e;
+`
+const Item4 = styled(BaseItem)`
+    width: 300px;
+    height: 300px;
+    margin-top: 1rem;
+    background-color: #d69e2e;
+`
+
+const Item5 = styled(BaseItem)`
+  width: 350px;
+margin-top: 1rem;
+background-color: #d69e2e;
+flex-basis:10%;
+`
+const Item6 = styled(BaseItem)`
+  width: 350px;
+margin-top: 1rem;
+background-color: #d69e2e;
+`
+
